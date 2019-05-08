cecf06676c6a93ac38a5e2b0c6361aa49ce05dcd
adjeteyboy <adjeieric4@yahoo.com>
2019-05-08 14:47:02 +0000

    made mobile menu

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var menu = document.getElementById('hamburgerIcon');
+// grab menu
+var mobileMenu = document.getElementById('mobileMenu');
+var body = document.body;
 
 function toggleMenu() {
   menu.classList.toggle('hamburger-icon__close-x');
+  mobileMenu.classList.toggle('mobile-menu--isActive');
+  body.classList.toggle('lock-scroll');
 }
